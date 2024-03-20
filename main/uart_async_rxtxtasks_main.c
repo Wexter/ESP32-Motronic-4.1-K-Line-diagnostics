9bc8484f0617fe99973cf337e936d6fa599dbea4
Vitaly Agapov <wesallenalex@gmail.com>
2024-03-20 17:40:38 +0300

    sync

--- main/uart_async_rxtxtasks_main.c
+++ main/uart_async_rxtxtasks_main.c
@@ -12,94 +12,95 @@
 #define UART_RXD_PIN GPIO_NUM_3
 #define UART_BAUD_RATE 8860
 
-#define KWP2000_ECHO_BYTE_TIMEOUT_MS 100
+#define KWP2000_ECHO_BYTE_TIMEOUT_TICKS (100 / portTICK_PERIOD_MS)
 #define KWP2000_ECHO_BYTE_RETRY_COUNT 3
 #define KWP2000_INIT_ECU_DST_ADDRESS 0x10
 
-#define K_LINE_RX_BUFFER_SIZE 2
-
 #define delay(ms) vTaskDelay(ms / portTICK_PERIOD_MS);
 
-const char nop_data = 0x09;
+const char nop_data[] = { 0x03, 0x00, 0x09, 0x00 };
 
-uint8_t connection_packet_id = 0;
+uint8_t kwp2000_session_packet_id = 0;
 
-bool kwp2000_send_byte(const char send_byte, bool wait_for_ack)
+bool k_line_send_byte(const char send_byte, bool wait_for_ack)
 {
+    uart_disable_rx_intr(UART_NUMBER);
     uart_write_bytes(UART_NUMBER, &send_byte, 1);
+    uart_enable_rx_intr(UART_NUMBER);
 
     if (!wait_for_ack)
         return true;
 
-    char received_bytes[K_LINE_RX_BUFFER_SIZE] = { 0x00, 0x00 };
+    char received_byte = 0x00;
 
     char bytes_read = 0;
 
     for (char retry_count = 0; retry_count < KWP2000_ECHO_BYTE_RETRY_COUNT; retry_count++)
     {
-        bytes_read = uart_read_bytes(UART_NUMBER, &received_bytes, K_LINE_RX_BUFFER_SIZE, KWP2000_ECHO_BYTE_TIMEOUT_MS / portTICK_PERIOD_MS);
+        bytes_read = uart_read_bytes(UART_NUMBER, &received_byte, 1, KWP2000_ECHO_BYTE_TIMEOUT_TICKS);
 
-        if (bytes_read > 0 && received_bytes[1] + send_byte == 0xFF)
+        if (bytes_read > 0 && received_byte + send_byte == 0xFF)
             return true;
     }
 
     return false;
 }
 
-int kwp2000_read_bytes(char* bytes, int bytes_count, bool send_ack)
+int k_line_read_bytes(char* bytes, int bytes_count, bool send_ack)
 {
     uint8_t bytes_read = 0,
         last_byte = 0;
 
     for (; bytes_read < bytes_count; bytes_read++)
     {
-        if(0 > uart_read_bytes(UART_NUMBER, &last_byte, bytes_count, KWP2000_ECHO_BYTE_TIMEOUT_MS / portTICK_PERIOD_MS))
+        if (0 < uart_read_bytes(UART_NUMBER, &last_byte, bytes_count, KWP2000_ECHO_BYTE_TIMEOUT_TICKS)) 
         {
             bytes[bytes_read] = last_byte;
 
-            kwp2000_send_byte(0xFF - last_byte, false);
+            k_line_send_byte(0xFF - last_byte, false);
         }
     }
 
     return bytes_read;
 }
 
-bool kwp2000_send_packet(const char * data, uint8_t data_length)
+bool kwp2000_send_packet(const char * packet)
 {
-    const uint8_t packet_length = data_length + 2; // packet id + data + packet end (0x03)
+    const uint8_t packet_length = packet[0];
 
     // Send packet length
-    if (!kwp2000_send_byte(packet_length, true))
+    if (!k_line_send_byte(packet_length, true))
         return false;
 
     // Send packet sequence number
-    if (!kwp2000_send_byte(connection_packet_id++, true))
+    if (!k_line_send_byte(kwp2000_session_packet_id++, true))
         return false;
 
     // Send packet data
-    for (uint8_t i = 0; i < data_length; i++)
-        if (!kwp2000_send_byte(data[i], true))
+    for (uint8_t i = 2; i < packet_length; i++)
+        if (!k_line_send_byte(packet[i], true))
             return false;
 
     // Send packet end mark 0x03 not wait ack
-    kwp2000_send_byte(0x03, false);
+    k_line_send_byte(0x03, false);
 
     return true;
 }
 
 inline void send_nop_packet()
 {
-    kwp2000_send_packet(&nop_data, 1);
+    kwp2000_send_packet(&nop_data);
 }
 
 // will read packet and return it's data
-int recv_packet(char * rx_buffer)
+int kwp2000_recv_packet(char * rx_buffer)
 {
-    // connection_packet_id++
+    kwp2000_session_packet_id++;
+
     return 0;
 }
 
-inline void kwp2000_()
+inline void kwp2000_start_full_speed()
 {
     const uart_config_t uart_config = {
         .baud_rate = UART_BAUD_RATE,
@@ -110,12 +111,12 @@ inline void kwp2000_()
         .source_clk = UART_SCLK_DEFAULT,
     };
     // We won't use a buffer for sending data.
-    uart_driver_install(UART_NUMBER, RX_BUF_SIZE, 0, 0, NULL, 0);
+    uart_driver_install(UART_NUMBER, UART_RX_BUF_SIZE, 0, 0, NULL, 0);
     uart_param_config(UART_NUMBER, &uart_config);
     uart_set_pin(UART_NUMBER, UART_TXD_PIN, UART_RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
 }
 
-inline void kwp2000_send_wakeup_slow()
+inline void kwp2000_send_slow_init_wakeup()
 {
     const uart_config_t uart_config = {
         .baud_rate = 5,
@@ -131,13 +132,22 @@ inline void kwp2000_send_wakeup_slow()
     uart_param_config(UART_NUMBER, &uart_config);
     uart_set_pin(UART_NUMBER, UART_TXD_PIN, UART_RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
 
-    kwp2000_send_byte(KWP2000_INIT_ECU_DST_ADDRESS, false);
+    k_line_send_byte(KWP2000_INIT_ECU_DST_ADDRESS, false);
 }
 
 void ecu_connect_kwp2000()
 {
-    kwp2000_send_wakeup_slow();
+    kwp2000_session_packet_id = 0;
 
+    kwp2000_send_slow_init_wakeup();
+
+    kwp2000_start_full_speed();
+
+    kwp2000_wait_handshake();
+
+    kwp2000_read_ecu_init_data();
+
+    // Start ecu send/recv queue
     // xTaskCreate();
 }
 
