dfb60c1cc25f62b0d8d6bd982eccb370dff18c67
Vitaly Agapov <wesallenalex@gmail.com>
2024-03-25 09:16:59 +0300

    sync

--- main/main.c
+++ main/main.c
@@ -12,59 +12,70 @@
 #define UART_RXD_PIN GPIO_NUM_3
 #define UART_BAUD_RATE 8860
 
-#define KWP2000_ECHO_BYTE_TIMEOUT_TICKS (100 / portTICK_PERIOD_MS)
+#define KWP2000_ECHO_BYTE_TIMEOUT_TICKS (100 / portTICK_PERIOD_MS) // 100ms
 #define KWP2000_ECHO_BYTE_RETRY_COUNT 3
 #define KWP2000_INIT_ECU_DST_ADDRESS 0x10
+#define KWP2000_PACKET_RECV_TIMEOUT_TICKS (100 / portTICK_PERIOD_MS) // 100ms
 
 #define delay(ms) vTaskDelay(ms / portTICK_PERIOD_MS);
 
-const char nop_data[] = { 0x03, 0x00, 0x09, 0x00 };
+const uint8_t no_data[] = { 0x03, 0x00, 0x09, 0x00, };
+const uint8_t request_throttle_pos[] = { 0x06, 0x00, 0x01, 0x01, 0x00, 0x20, 0x03, };
+const uint8_t request_engine_rpm[] = { 0x06, 0x00, 0x01, 0x01, 0x00, 0x3a, 0x03, };
+const uint8_t request_engine_load[] = { 0x06, 0x00, 0x01, 0x01, 0x00, 0x42, 0x03, };
+const uint8_t request_injection_time[] = { 0x06, 0x00, 0x01, 0x02, 0x00, 0x62, 0x03, };
+const uint8_t request_airflow_meter_value[] = { 0x04, 0x00, 0x08, 0x00, 0x03, };
+const uint8_t request_battery_voltage[] = { 0x04, 0x00, 0x08, 0x01, 0x03, };
+const uint8_t request_intake_air_temp[] = { 0x04, 0x00, 0x08, 0x02, 0x03, };
+const uint8_t request_coolant_temp[] = { 0x04, 0x00, 0x08, 0x03, 0x03, };
+const uint8_t request_co_pot_value[] = { 0x04, 0x00, 0x08, 0x04, 0x03, };
+const uint8_t request_o2_sensor_value[] = { 0x04, 0x00, 0x08, 0x05, 0x03, };
+const uint8_t request_ignition_time[] = { 0x04, 0x00, 0x08, 0x07, 0x03, };
 
 uint8_t kwp2000_session_packet_id = 0;
 
-bool k_line_send_byte(const char send_byte, bool wait_for_ack)
+bool k_line_send_byte(const uint8_t send_byte, bool wait_echo_byte)
 {
     uart_disable_rx_intr(UART_NUMBER);
     uart_write_bytes(UART_NUMBER, &send_byte, 1);
     uart_enable_rx_intr(UART_NUMBER);
 
-    if (!wait_for_ack)
+    if (!wait_echo_byte)
         return true;
 
-    char received_byte = 0x00;
+    uint8_t echo_byte = 0x00,
+        bytes_read = 0;
 
-    char bytes_read = 0;
-
-    for (char retry_count = 0; retry_count < KWP2000_ECHO_BYTE_RETRY_COUNT; retry_count++)
+    for (uint8_t retry_count = 0; retry_count < KWP2000_ECHO_BYTE_RETRY_COUNT; retry_count++)
     {
-        bytes_read = uart_read_bytes(UART_NUMBER, &received_byte, 1, KWP2000_ECHO_BYTE_TIMEOUT_TICKS);
+        bytes_read = uart_read_bytes(UART_NUMBER, &echo_byte, 1, KWP2000_ECHO_BYTE_TIMEOUT_TICKS);
 
-        if (bytes_read > 0 && received_byte + send_byte == 0xFF)
+        if (bytes_read > 0 && echo_byte == ~send_byte)
             return true;
     }
 
     return false;
 }
 
-int k_line_read_bytes(char* bytes, int bytes_count, bool send_ack)
+int k_line_read_bytes(uint8_t* bytes, int bytes_count, bool send_echo, TickType_t read_timeout)
 {
     uint8_t bytes_read = 0,
         last_byte = 0;
 
-    for (; bytes_read < bytes_count; bytes_read++)
-    {
-        if (0 < uart_read_bytes(UART_NUMBER, &last_byte, bytes_count, KWP2000_ECHO_BYTE_TIMEOUT_TICKS)) 
-        {
-            bytes[bytes_read] = last_byte;
+    do {
+        if (0 >= uart_read_bytes(UART_NUMBER, &last_byte, 1, read_timeout))
+            return bytes_read; // no byte received
 
-            k_line_send_byte(0xFF - last_byte, false);
-        }
-    }
+        bytes[bytes_read] = last_byte;
+
+        if (bytes_read + 1 < bytes_count) // Send echo for all except last one
+            k_line_send_byte(~last_byte, false);
+    } while (++bytes_read < bytes_count);
 
     return bytes_read;
 }
 
-bool kwp2000_send_packet(const char * packet)
+bool kwp2000_send_packet(const uint8_t* packet)
 {
     const uint8_t packet_length = packet[0];
 
@@ -77,46 +88,32 @@ bool kwp2000_send_packet(const char * packet)
         return false;
 
     // Send packet data
-    for (uint8_t i = 2; i < packet_length; i++)
+    // Skip first 2 bytes (length & packet id) and last byte (EOP)
+    for (uint8_t i = 2; i < packet_length - 1; i++)
         if (!k_line_send_byte(packet[i], true))
             return false;
 
-    // Send packet end mark 0x03 not wait ack
+    // Send packet end mark 0x03 without waiting for echo
     k_line_send_byte(0x03, false);
 
     return true;
 }
 
-inline void send_nop_packet()
+void send_nop_packet()
 {
-    kwp2000_send_packet(&nop_data);
+    kwp2000_send_packet(no_data);
 }
 
 // will read packet and return it's data
-int kwp2000_recv_packet(char * rx_buffer)
+int kwp2000_recv_packet(uint8_t * rx_buffer)
 {
+
     kwp2000_session_packet_id++;
 
     return 0;
 }
 
-inline void kwp2000_start_full_speed()
-{
-    const uart_config_t uart_config = {
-        .baud_rate = UART_BAUD_RATE,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_DEFAULT,
-    };
-    // We won't use a buffer for sending data.
-    uart_driver_install(UART_NUMBER, UART_RX_BUF_SIZE, 0, 0, NULL, 0);
-    uart_param_config(UART_NUMBER, &uart_config);
-    uart_set_pin(UART_NUMBER, UART_TXD_PIN, UART_RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-}
-
-inline void kwp2000_send_slow_init_wakeup()
+void kwp2000_send_slow_init_wakeup()
 {
     const uart_config_t uart_config = {
         .baud_rate = 5,
@@ -135,7 +132,38 @@ inline void kwp2000_send_slow_init_wakeup()
     k_line_send_byte(KWP2000_INIT_ECU_DST_ADDRESS, false);
 }
 
-void ecu_connect_kwp2000()
+void kwp2000_start_full_speed()
+{
+    const uart_config_t uart_config = {
+        .baud_rate = UART_BAUD_RATE,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    // We won't use a buffer for sending data.
+    uart_driver_install(UART_NUMBER, UART_RX_BUF_SIZE, 0, 0, NULL, 0);
+    uart_param_config(UART_NUMBER, &uart_config);
+    uart_set_pin(UART_NUMBER, UART_TXD_PIN, UART_RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+}
+
+bool kwp2000_wait_handshake()
+{
+    uint8_t rx_buffer[2];
+
+    if (2 > k_line_read_bytes(rx_buffer, 2, true, KWP2000_PACKET_RECV_TIMEOUT_TICKS))
+        return false;
+
+    return true;
+}
+
+void kwp2000_read_ecu_init_data()
+{
+
+}
+
+void ecu_connect_kwp2000(void *)
 {
     kwp2000_session_packet_id = 0;
 
